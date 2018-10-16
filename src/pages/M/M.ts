684384a3c784e43aa708174f46e3e4756940376d
Shafran Naizer <shafran@Shafrans-MacBook-Pro.local>
2018-10-16 22:49:41 +0530

    App TEST

---
+++ src/pages/M/M.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'M-page',
+  templateUrl: 'M.html'
+})
+export class M {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
